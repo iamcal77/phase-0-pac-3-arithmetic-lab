fed9532333048037daef352f85953796cb604f1e
iamcal77 <+>
2023-06-12 13:49:27 +0300

    assignment done

---
+++ index.js
@@ -0,0 +1,90 @@
+function add() {
+    // we'll fill this in shortly
+  }
+
+  function subtract() {
+    // we'll fill this in shortly
+  }
+
+  function multiply() {
+    // we'll fill this in shortly
+  }
+
+  function divide() {
+    // we'll fill this in shortly
+  }
+  function add() {
+    return "something";
+  }
+  function add(a, b) {
+    return "something";
+  }
+  function add(a, b) {
+    return a + b;
+  }
+  function subtract() {
+    return "something";
+  }
+  function subtract(a,b) {
+    return "something";
+  }
+  function subtract(a,b) {
+    return a-b;
+  }
+  function multiply() {
+    return "something";
+  }
+  function multiply(a,b) {
+    return "something";
+  }
+  function multiply(a,b) {
+    return a*b;
+  }
+  function multiply() {
+    // we'll fill this in shortly
+  }
+  function multiply(a,b) {
+    return "something"
+  }
+  function multiply(a,b) {
+    return a*b;
+  }
+  function divide(a,b) {
+    return "something"
+  }
+  function divide(a,b) {
+    return a/b;
+  }
+  function increment(n) {
+    return n + 1;
+  }
+  function decrement(n) {
+    return n-1;
+  }
+  function makeInt(n) {
+    return parseInt(n, 10);
+  }
+  function preserveDecimal(n) {
+    return parseFloat(n);
+  }
+  function preserveDecimal(n) {
+    return parseFloat(n);
+  }
+  function preserveDecimal(n) {
+    return parseFloat(n);
+  }
+  function preserveDecimal(n) {
+    return parseFloat(n);
+  }
+  function preserveDecimal(n) {
+    return parseFloat(n);
+  }
+  function preserveDecimal(n) {
+    return parseFloat(n);
+  }
+  function preserveDecimal(n) {
+    return parseFloat(n);
+  }
+  
+
+  
